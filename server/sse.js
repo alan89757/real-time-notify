50d1b869172ec6e6cf2b983b854dd6ac4aa3fac1
alan liang <843349919@qq.com>
2022-09-07 22:09:10 +0800

    express sse

--- server/sse.js
+++ server/sse.js
@@ -1,63 +1,58 @@
-const http = require('http')
-const url = require('url')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
 
-const events = []
-const clients = new Set()
-let latestTimestamp = Date.now()
+const events = []; // 存储事件
+const clients = new Set(); // 存储客户端的请求
+let latestTimestamp = Date.now();
 
 const headers = {
-  // 告诉HTTP连接，它是一个event-stream的请求
-  'Content-Type': 'text/event-stream',
-  // 保持HTTP连接不断开
+  'Content-type': 'text/event-stream',
   'Connection': 'keep-alive',
   'Cache-Control': 'no-cache',
-  'Access-Control-Allow-Origin': '*',
-  "Origin": '*'
 }
 
 const EventProducer = () => {
+  console.log(111)
   const event = {
     id: Date.now(),
-    timestamp: Date.now()
+    timestamp: Date.now(),
   }
-  events.push(event)
-  latestTimestamp = event.timestamp
-
+  events.push(event);
+  latestTimestamp = event.timestamp;
   clients.forEach(client => {
-    client.resp.write(`id: ${(new Date()).toLocaleTimeString()}\n`)
-    // 后面的两个\n\n一定要有，可以理解为服务端先客户端推送信息的特殊格式
-    client.resp.write(`data: ${JSON.stringify(events.filter(event => event.timestamp > client.timestamp))}\n\n`)
-    client.timestamp = latestTimestamp
-  })
+    console.log('client---', client.res);
+    client.res.write(`id: ${(new Date()).toLocaleTimeString()}\n`);
+    client.res.write(`data: ${JSON.stringify(events.filter(event=>event.timestamp > client.timestamp))}\n\n`);
+    client.timestamp = latestTimestamp;
+  });
+
 }
 
-// 每5秒生成一个新的事件
 setInterval(() => {
-  EventProducer()
-}, 5000)
+  EventProducer();
+}, 5000);
 
-const server = http.createServer((req, resp) => {
-  const urlParsed = url.parse(req.url, true)
+app.get('/subscribe', function (req, res) {
+  res.header(headers);
+  res.write(`id:${(new Date()).toLocaleTimeString()}\n`);
+  res.write(`data:${JSON.stringify(events)}\n\n`);
+  // setInterval(() => {
+  //   res.write(`data: ${(new Date()).toLocaleTimeString()}\n\n`);
+  // }, 2000);
 
-  if (urlParsed.pathname == '/subscribe') {
-    resp.writeHead(200, headers)
-    
-    // 发送现存事件
-    resp.write(`id: ${(new Date()).toLocaleTimeString()}\n`)
-    resp.write(`data: ${JSON.stringify(events)}\n\n`)
-   
-    const client = {
-      timestamp: latestTimestamp,
-      resp
-    }
-    
-    clients.add(client)
-    req.on('close', () => {
-      clients.delete(client)
-    })
+  const client = {
+    timestamp: latestTimestamp,
+    res
   }
-})
+  clients.add(client);
+  req.on('close', () => {
+    clients.delete(client);
+  })
 
-server.listen(8080, () => {
-  console.log('server is up')
+});
+
+app.listen(8080, () => {
+  console.log('Server is up');
 })
